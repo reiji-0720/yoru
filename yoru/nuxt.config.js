acf6ecfee4754137aa00515e71b9f60915f66f78
reiji-0720 <55520463+reiji-0720@users.noreply.github.com>
2020-11-02 17:38:56 +0900

    基盤設定

--- yoru/nuxt.config.js
+++ yoru/nuxt.config.js
@@ -52,7 +52,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.green.accent3,
+        },
+        light:{
+          background:"#1F2E35"
         }
       }
     }
---
+++ yoru/pages/signUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.light.background}" >
+      <v-container>
+        <img src="~/assets/main_logo.png" width="100%" class="px-8">
+      </v-container>
+  </v-app>
+</template>
+<script>
+export default {
+  title:"Sign up"
+}
+</script>
+
+<style scoped>
+</style>
